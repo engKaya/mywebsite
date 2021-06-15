5d2ef87f8116554bca3de9044e8cf73be5a2401b
engKaya <36958369+engKaya@users.noreply.github.com>
2021-06-15 23:46:49 +0300

    2

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,42 @@
+const bars = document.getElementsByClassName('bars')
+toUp()
 setNavUpOnNav()
+
+function toUp(){
+    $('.to-up').on('click', () => {
+        if(this.scrollY > 500) {
+            window.scrollTo(0,0)
+        }
+    })
+}
+
+
+/* navbar area */
 
 $(function(){
     $(window).scroll(function() {
-       this.setNavUpOnNav()
+        this.setNavUpOnNav()
+        if(this.scrollY > 500)
+            $('.to-up').addClass('show')
+        else
+            $('.to-up').removeClass('show')
+
     })
 })
+
+
+let activeToggle = false
+
+$(function(){
+    document.querySelectorAll('.menu-link').forEach(item => {
+        item.addEventListener('click', event => {
+            activeToggle = false
+            $('.menu').removeClass('active')
+            $('.menu-btn i').removeClass('active')
+        })
+    })
+})
+
 
 function setNavUpOnNav() {
     if(this.scrollY > 20)
@@ -14,7 +46,6 @@ function setNavUpOnNav() {
 }
 
 
-let activeToggle = false
 
 $('.fa-bars').on('click', function(){
     activeToggle = !activeToggle
@@ -28,3 +59,31 @@ $('.fa-bars').on('click', function(){
     }
 
 })
+
+/* about-section area */
+
+$('.down-cv').on('click', function (e) {
+    e.preventDefault()
+    window.location.href = 'cv.pdf'
+})
+
+/* skills-section area */
+let skillSectionSeen = false
+
+$(window).on('scroll',()=>{
+    if(this.scrollY > 900 && !skillSectionSeen ){
+        skillSectionSeen = true
+        for(let i=0; i<bars.length; i++){
+            const percent = bars[i].getElementsByClassName('info')[0].getElementsByTagName('span')[1].innerHTML
+            const line = bars[i].querySelector('.line').querySelector('.percent')
+            line.style.width = `${percent}`
+        }
+    }else if (this.scrollY < 900 ) {
+        skillSectionSeen = false
+        for(let i=0; i<bars.length; i++){
+            const line = bars[i].querySelector('.line').querySelector('.percent')
+            line.style.width = `0%`
+        }
+    }
+})
+
